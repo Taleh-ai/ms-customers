98e316586892812dab3e284f1cc82c59dca83912
TalehRza <58006591+Taleh-ai@users.noreply.github.com>
2023-07-13 11:30:50 +0400

    Update OrderEntity.java
    update colmn name

--- src/main/java/com/example/mscustomers/entity/OrderEntity.java
+++ src/main/java/com/example/mscustomers/entity/OrderEntity.java
@@ -14,13 +14,24 @@ public class OrderEntity {
     @ManyToOne
     @JoinColumn(name ="customer_id")
     private CustomerEntity userEntity;
+    
     private int quantity;
+    @Column(name = "total_price")
     private double totalPrice;
+    
+    @Column(name = "order_date")
     private LocalDate orderDate;
+    @Column(name = "delivered_date")
     private LocalDate deliveredDate;
+    @Column(name = "cancel_date")
     private LocalDate cancelDate;
+    @Column(name = "shipping_date")
     private LocalDate shippingDate;
     private String description;
+    @Column(name = "product_id")
     private Long productId;
-    private String status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "order_status")
+    private OrderStatus  status;
 }
