610c29f0dbd1e9b9ba75c0cf218c855fa700d2c2
TalehRza <58006591+Taleh-ai@users.noreply.github.com>
2023-07-13 14:48:47 +0400

    Update CustomerResponseDto.java
    write fields

--- src/main/java/com/example/mscustomers/dto/response/CustomerResponseDto.java
+++ src/main/java/com/example/mscustomers/dto/response/CustomerResponseDto.java
@@ -1,4 +1,10 @@
 package com.example.mscustomers.dto.response;
-
-public class CustomerResponseDto {
+@Data
+  public class CustomerResponseDto {
+    private Long CustomerId
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String phoneNumber;
 }
