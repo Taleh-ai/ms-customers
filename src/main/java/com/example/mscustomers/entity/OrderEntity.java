3205ed4b1298adaca4035c8a4c05fa99248bf1a7
TalehRza <58006591+Taleh-ai@users.noreply.github.com>
2023-07-13 10:54:10 +0400

    Update OrderEntity.java
    write order entity fields

--- src/main/java/com/example/mscustomers/entity/OrderEntity.java
+++ src/main/java/com/example/mscustomers/entity/OrderEntity.java
@@ -5,19 +5,22 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "orders")
 public class OrderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name ="customer_id")
     private CustomerEntity userEntity;
-
-    // List of Product entities
-
+    private int quantity;
     private double totalPrice;
     private LocalDate orderDate;
+    private LocalDate deliveredDate;
+    private LocalDate cancelDate;
+    private LocalDate shippingDate;
+    private String description;
+    private Long productId;
     private String status;
-
-    // Constructors, getters, and setters
 }
