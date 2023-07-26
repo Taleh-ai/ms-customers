3cefd802b1be4fde999ae99e56b2cd7898b67bcf
Taleh-ai <talehrzayev000@gmail.com>
2023-07-26 08:59:44 +0400

    fix email service

--- src/main/java/com/example/mscustomers/MsCustomersApplication.java
+++ src/main/java/com/example/mscustomers/MsCustomersApplication.java
@@ -2,8 +2,10 @@ package com.example.mscustomers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MsCustomersApplication {
 
     public static void main(String[] args) {
