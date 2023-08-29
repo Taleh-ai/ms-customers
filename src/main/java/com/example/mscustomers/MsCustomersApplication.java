10bcdb14397dbd062d9a78289287adfe6db9af29
TalehRza <talehrzayev000@gmail.com>
2023-08-30 01:32:49 +0400

    fix feign client

--- src/main/java/com/example/mscustomers/MsCustomersApplication.java
+++ src/main/java/com/example/mscustomers/MsCustomersApplication.java
@@ -2,9 +2,11 @@ package com.example.mscustomers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsCustomersApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/mscustomers/client/ProductServiceClient.java
+++ src/main/java/com/example/mscustomers/client/ProductServiceClient.java
@@ -1,19 +1,21 @@
-//package com.example.mscustomers.client;
-//
-//import com.example.mscustomers.dto.response.ProductResponseDto;
-//import com.example.mscustomers.handler.SuccessDetails;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//
-//import java.util.List;
-//
-//@FeignClient(name = "msdealer",url = "http://localhost:8081")
-//public interface ProductServiceClient {
-//
-//    @GetMapping("/v1/product/{id}")
-//    ResponseEntity<SuccessDetails<ProductResponseDto>> getProductById(@PathVariable("id") Long id);
-//
-//    @GetMapping("/v1/product")
-//    ResponseEntity<SuccessDetails<List<ProductResponseDto>>> getAllProducts();
-//}
+package com.example.mscustomers.client;
+
+import com.example.mscustomers.dto.response.ProductResponseDto;
+
+import com.example.mscustomers.exception.handler.SuccessDetails;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "msdealer",url = "http://localhost:8081")
+public interface ProductServiceClient {
+
+    @GetMapping("v1/product-feign/{id}")
+    ResponseEntity<SuccessDetails<ProductResponseDto>> getProductById(@PathVariable("id") Long id);
+
+    @GetMapping("v1/product-feign")
+    ResponseEntity<SuccessDetails<List<ProductResponseDto>>> getAllProducts();
+}
